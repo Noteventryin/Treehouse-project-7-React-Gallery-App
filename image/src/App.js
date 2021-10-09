ab66f263b83fc14369893bcffb26d2c7d3a5d559
Noteventryin <tony1730280546@gmail.com>
2021-10-08 22:34:10 -0400

    Start file

---
+++ image/src/App.js
@@ -0,0 +1,30 @@
+import React from 'react';
+
+//App components.
+import config from './config'
+
+
+
+
+
+
+
+
+
+
+
+const App = () => (
+  
+);
+
+
+
+
+
+
+
+
+
+
+
+export default App;
---
+++ image/src/components/config.js
@@ -0,0 +1,8 @@
+const apiKey = 'e5b6f31120eab32dcce102ce44ce4f93';
+
+
+
+
+
+
+export default apiKey;
